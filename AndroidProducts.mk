1672eeff5a206b698b238a19fba6dc13d5a6a479
allworkdone <raj635321@gmail.com>
2021-04-06 17:42:02 +0530

    Evolution-X BringUP Updated Build FP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1801.mk
+    $(LOCAL_DIR)/evolution_RMX1801.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_RMX1801-user \
-    aosp_RMX1801-userdebug \
-    aosp_RMX1801-eng
+    evolution_RMX1801-user \
+    evolution_RMX1801-userdebug \
+    evolution_RMX1801-eng
--- evolution_RMX1801.mk
+++ evolution_RMX1801.mk
@@ -14,17 +14,13 @@
 # limitations under the License.
 #
 
-# Inherit some common AOSP stuff
+# Inherit some common lineage stuff
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common aosp-ppui stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common evolution stuff
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-CUSTOM_BUILD_TYPE := OFFICIAL
-PRODUCT_PROPERTY_OVERRIDES += \
-      ro.pixelplusui.maintainer=allworkdone
-TARGET_FACE_UNLOCK_SUPPORTED=true
 
 # Inherit from Realme RMX1801
 $(call inherit-product, device/realme/RMX1801/device.mk)
@@ -32,7 +28,7 @@ $(call inherit-product, device/realme/RMX1801/device.mk)
 # Set Shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-PRODUCT_NAME := aosp_RMX1801
+PRODUCT_NAME := evolution_RMX1801
 PRODUCT_DEVICE := RMX1801
 PRODUCT_MANUFACTURER := OPPO
 PRODUCT_BRAND := oppo
@@ -48,4 +44,11 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1801" \
     PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1.191014.001 eng.root.20200727.144331 release-keys"
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
+
+#Evolution-X-stuff
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+EVO_MAINTAINER := allworkdone
